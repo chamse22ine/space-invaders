3f15dcc4db710d0169bf77c367135e09c001f04b
chamse22ine <imadaadour@gmail.com>
2026-01-03 10:35:19 +0100

    Clear

--- 3rdParty/SDL3_image/build/CMakeFiles/SDL3_image-shared.dir/src/IMG_WIC.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/SDL3_image-shared.dir/src/IMG_WIC.c.o: \
- /home/chamseddine/Documents/cours/SAE/L3/c-avance/space-invaders/3rdParty/SDL3_image/src/IMG_WIC.c \
- /usr/include/stdc-predef.h
